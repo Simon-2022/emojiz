d4dc009c80e2909b2b3ab5f671b070b2c9c0c379
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2022-10-24 17:42:28 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Prediction1 = "";
+Prediction2 = "";
+Webcam.set({
+    width: 350,
+    height: 300,
+    Image_format: 'png',
+    png_quality: 100,
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifierer('https://teachablemachine.withgoogle.com/models/ejzxDfXYk/model.json', modelLoaded);
+function modelLoaded() { console.log("Model Loaded") }
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + Prediction1;
+    speak_data_2 = "The second prediction is " + Prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
